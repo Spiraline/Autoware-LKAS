bda2d4aca50614d911f5f1969c2a533046055a0d
Spiraline <spiraline@naver.com>
2021-03-04 19:01:08 +0900

    Vanilla Complete

--- rubis_ws/src/rubis_pkg/src/lidar_republisher.cpp
+++ rubis_ws/src/rubis_pkg/src/lidar_republisher.cpp
@@ -6,15 +6,16 @@ static ros::Subscriber sub;
 static ros::Publisher pub;
 
 void points_cb(const sensor_msgs::PointCloud2& msg){
-    sensor_msgs::PointCloud2 out;
-    out = msg;
-    out.header.stamp = ros::Time::now();
-    pub.publish(out);
+    sensor_msgs::PointCloud2 msg_with_intensity = msg;
+    msg_with_intensity.fields.at(3).datatype = 7;
+
+    msg_with_intensity.header.stamp = ros::Time::now();
+    pub.publish(msg_with_intensity);
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "lidar_republisher");
-    ros::NodeHandle nh;    
+    ros::NodeHandle nh;
     std::string input_topic;
     std::string output_topic;
 
@@ -25,8 +26,8 @@ int main(int argc, char** argv){
     nh.param<std::string>(input_topic_name, input_topic, "/points_raw_origin");
     nh.param<std::string>(output_topic_name, output_topic, "/points_raw");
 
-    sub = nh.subscribe(input_topic, 1, points_cb);
-    pub = nh.advertise<sensor_msgs::PointCloud2>(output_topic, 1);    
+    sub = nh.subscribe(input_topic, 1, points_cb);    
+    pub = nh.advertise<sensor_msgs::PointCloud2>(output_topic, 1);
 
     while(ros::ok())
         ros::spin();
