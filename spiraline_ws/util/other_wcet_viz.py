c0ae7cb9ee77825ece2bba31e0c75ae44e627229
Spiraline <spiraline@naver.com>
2022-02-15 16:32:44 +0900

    imm_ukf_pda typo resolved

--- spiraline_ws/util/other_wcet_viz.py
+++ spiraline_ws/util/other_wcet_viz.py
@@ -15,13 +15,23 @@ if __name__ == '__main__':
     node_name = file.split('.')[0]
     if node_name == "ndt_matching":
       continue
-    wcet = 0
+    exec_arr = []
     with open(csv_log_dir + file, 'r') as f:
       rr = csv.reader(f)
       for row in rr:
         exec_t = float(row[1]) - float(row[0])
-        if exec_t > wcet:
-          wcet = exec_t
+        if len(exec_arr) > 10:
+          exec_arr.sort()
+          if exec_arr[0] < exec_t:
+            del exec_arr[0]
+            exec_arr.append(exec_t)
+        else:
+          exec_arr.append(exec_t)
+      # print(node_name, [round(e*1000, 2) for e in exec_arr])
+      wcet = exec_arr[0]
+      for candidate in exec_arr[1:]:
+        if candidate < wcet * 1.5:
+          wcet = candidate
     table_data.append([node_name, round(wcet*1000, 2)])
 
   column_labels=["Node Name", "WCET (ms)"]
